63d1f246847fe262947f1b704f159b8666c2e547
Matiasalexander <matiasalexander89@gmail.com>
2026-06-03 17:41:28 -0500

    creation of modelos routes  and const in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const marcasRoutes = require("./routes/marcas.routes");
 const tipoEquipoRoutes = require("./routes/tipoEquipo.routes");
 const estatusRoutes = require("./routes/estatus.routes");
 const procesadoresRoutes = require("./routes/procesadores.routes");
+const modelosRoutes = require("./routes/modelos.routes");
 require("dotenv").config();
 
 require("./config/db");
@@ -20,6 +21,7 @@ app.use("/api/marcas", marcasRoutes);
 app.use("/api/tipo-equipo", tipoEquipoRoutes);
 app.use("/api/estatus", estatusRoutes);
 app.use("/api/procesadores", procesadoresRoutes);
+app.use("/api/modelos", modelosRoutes);
 
 
 app.get("/", (req, res) => {
