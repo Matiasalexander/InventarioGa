245c5db4c4544c82e88e15ff3633fa941f94aac2
Matiasalexander <matiasalexander89@gmail.com>
2026-06-24 15:04:47 -0500

    update routes auth , implementation of olvide y reset password

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const {
   login,
-  registrarUsuario
+  registrarUsuario,
+  olvidePassword,
+  resetPassword
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
@@ -10,5 +12,9 @@ const router = express.Router();
 router.post("/login", login);
 
 router.post("/registro", registrarUsuario);
+
+router.post("/olvide-password", olvidePassword);
+
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
