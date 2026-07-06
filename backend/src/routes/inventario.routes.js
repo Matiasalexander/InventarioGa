233c0b03996c34e8634d9050a1e410df0b4233bf
Matiasalexander <matiasalexander89@gmail.com>
2026-07-06 16:25:34 -0500

    actualizacion de controladores por error de excel

--- backend/src/routes/inventario.routes.js
+++ backend/src/routes/inventario.routes.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {
-obtenerInventario,
-obtenerInventarioPorId,
-crearInventario,
-actualizarInventario,
-eliminarInventario,
-obtenerArbolUnidades,
-exportarInventarioExcel
 
-}= require("../controllers/inventario.controller");
+const {
+  obtenerInventario,
+  obtenerInventarioPorId,
+  crearInventario,
+  actualizarInventario,
+  eliminarInventario,
+  obtenerArbolUnidades,
+  exportarInventarioExcel
+} = require("../controllers/inventario.controller");
+
 router.get("/arbol-unidades", obtenerArbolUnidades);
 router.get("/exportar-excel", exportarInventarioExcel);
 
 router.get("/", obtenerInventario);
-router.get("/:id",obtenerInventarioPorId);
+router.get("/:id", obtenerInventarioPorId);
 router.post("/", crearInventario);
-router.put("/:id",actualizarInventario);
-router.delete("/:id",eliminarInventario);
+router.put("/:id", actualizarInventario);
+router.delete("/:id", eliminarInventario);
 
 module.exports = router;
