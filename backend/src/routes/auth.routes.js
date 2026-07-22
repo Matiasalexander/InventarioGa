19134ff4a8e62f2ee86acc3f90ea27f775bb711f
Matiasalexander <matiasalexander89@gmail.com>
2026-07-22 11:40:46 -0500

    conexiones- de token

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -8,6 +8,9 @@ const {
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
+const {
+  verificarToken
+} = require("../middleware/auth.middleware");
 
 router.post("/login", login);
 
@@ -16,5 +19,12 @@ router.post("/registro", registrarUsuario);
 router.post("/olvide-password", olvidePassword);
 
 router.post("/reset-password", resetPassword);
+
+router.get("/validar-token", verificarToken, (req, res) => {
+  return res.json({
+    message: "Token válido",
+    usuario: req.usuario
+  });
+});
 
 module.exports = router;
