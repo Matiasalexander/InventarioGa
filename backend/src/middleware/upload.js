7363241933ff24aa3a3c54cb29e4c2a668dc1fe4
becariosistemas-cloud <becario.sistemas@grupoandersons.com>
2026-07-22 15:17:13 -0500

    modificación del archivo uploads

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -1,11 +1,4 @@
 const multer = require("multer");
-const path = require("path");
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{cb(null,"uploads/inventario");},
-    filename:(req,file,cb)=>{const extension = path.extname(file.originalname);
-    const nombre = "equipo_"+Date.now()+extension;
-    cb(null,nombre);
-    }
-});
-const upload = multer({storage:storage});
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 module.exports=upload;
