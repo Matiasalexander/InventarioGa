4f555ad704816a67cdc40d048422accadf8f5b18
Matiasalexander <matiasalexander89@gmail.com>
2026-07-24 11:08:22 -0500

    bloqueo por catalogos - update

--- backend/src/routes/catalogos.routes.js
+++ backend/src/routes/catalogos.routes.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const { obtenerCatalogos } = require("../controllers/catalogos.controller");
-const { verificarToken } = require("../middleware/auth.middleware");
-const { verificarPermiso } = require("../middleware/permisos.middleware");
+const {
+  obtenerCatalogos
+} = require("../controllers/catalogos.controller");
+
+const {
+  verificarToken
+} = require("../middleware/auth.middleware");
+
+const {
+  verificarPermiso
+} = require("../middleware/permisos.middleware");
+
 router.get(
   "/",
   verificarToken,
-  verificarPermiso("catalogos.ver"),
+  verificarPermiso("catalogos.consultar"),
   obtenerCatalogos
 );
 
