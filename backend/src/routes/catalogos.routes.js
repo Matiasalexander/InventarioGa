f97afe14ea7d10e7f83604cf60dc62a18da1c92b
Matiasalexander <matiasalexander89@gmail.com>
2026-07-24 10:15:27 -0500

    implementacion en catalogos

--- backend/src/routes/catalogos.routes.js
+++ backend/src/routes/catalogos.routes.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { obtenerCatalogos } = require("../controllers/catalogos.controller");
-
-router.get("/", obtenerCatalogos);
+const { verificarToken } = require("../middleware/auth.middleware");
+const { verificarPermiso } = require("../middleware/permisos.middleware");
+router.get(
+  "/",
+  verificarToken,
+  verificarPermiso("catalogos.ver"),
+  obtenerCatalogos
+);
 
 module.exports = router;
